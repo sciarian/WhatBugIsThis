256d916e57aafcef6aafb9bf06265d083d724536
user144818 <user144818@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-11-27 20:01:41 -0800

    first commit

---
+++ WhatBugIsThisTests/WhatBugIsThisTests.swift
@@ -0,0 +1,36 @@
+//
+//  WhatBugIsThisTests.swift
+//  WhatBugIsThisTests
+//
+//  Created by user144818 on 11/22/18.
+//  Copyright © 2018 user144818. All rights reserved.
+//
+
+import XCTest
+@testable import WhatBugIsThis
+
+class WhatBugIsThisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
